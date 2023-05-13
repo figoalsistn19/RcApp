672c371e4a9024f7b6631b6279d24f3c63a03742
Annisa <annisarosdiyana17@gmail.com>
2023-05-13 22:56:58 +0700

    add getItems fun in data layer

--- app/src/main/java/com/appbygox/rcapp/MainActivity.kt
+++ app/src/main/java/com/appbygox/rcapp/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.appbygox.rcapp.databinding.ActivityMainBinding
-import com.appbygox.rcapp.login.LoginActivity
+import com.appbygox.rcapp.ui.login.LoginActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
